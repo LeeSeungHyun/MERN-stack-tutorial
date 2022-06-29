21d0276615feb6955c6c53a629df1b966e17a52b
LeeSeungHyun <iop2480@naver.com>
2022-06-29 15:59:42 +0900

    add controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const worktoutRoutes = require('./routes/workouts')
 
 // express app
@@ -16,8 +17,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', worktoutRoutes)
 
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
