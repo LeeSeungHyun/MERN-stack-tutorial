bd935382b98ad784d10fff7b4fdc14d0c10e1c16
catenoid-shlee <sh.lee@catenoid.net>
2022-07-22 13:18:14 +0900

    signing up & hashing password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,12 +2,21 @@ const User = require('../models/userModel')
 
 // login user
 const loginUser = async (req, res) => {
-  console.log(123124124)
   res.json({ mssg: 'login user' })
 }
 
 // signup user
 const signupUser = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.signup(email, password)
+
+    res.status(200).json({ email, user })
+  } catch (error) {
+    res.status(400).json({ error:  error.message })
+  }
+
   res.json({ mssg: 'signup user' })
 }
 
